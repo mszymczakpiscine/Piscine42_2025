0fb5acce35e5fcfbc30a945c9eaaf3a1caf83a5f
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-01-20 21:56:17 +0100

    Update ft_str_is_lowercase.c

--- C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex04/ft_str_is_lowercase.c
+++ C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex04/ft_str_is_lowercase.c
@@ -25,3 +25,14 @@ int	ft_str_is_lowercase(char *str)
 	}
 	return (1);
 }
+/*
+#include <stdio.h>
+
+int    main(void)
+{
+    int islower;
+    char a[] = "123";
+    islower = ft_str_is_lowercase(a);
+    printf("%d\n", islower);
+}
+*/
