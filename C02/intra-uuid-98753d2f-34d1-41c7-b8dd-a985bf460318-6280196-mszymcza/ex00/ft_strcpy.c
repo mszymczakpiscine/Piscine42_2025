eb954b3a80e9237e35fa4e532c1aa5bc720238bc
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-01-20 21:53:00 +0100

    Update ft_strcpy.c

--- C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex00/ft_strcpy.c
+++ C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex00/ft_strcpy.c
@@ -24,3 +24,15 @@ char	*ft_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char dest[] = "";
+
+	printf("%lu\n", strlen(dest));	
+	ft_strcpy(dest, "Super mega test!");
+	printf("%s", dest);	
+}
+*/
