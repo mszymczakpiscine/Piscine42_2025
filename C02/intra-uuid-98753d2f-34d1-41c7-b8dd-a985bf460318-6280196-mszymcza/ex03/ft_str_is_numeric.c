168463b2587d10e487fcf6437138d33f800c46d2
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-01-20 21:55:57 +0100

    Update ft_str_is_numeric.c

--- C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex03/ft_str_is_numeric.c
+++ C02/intra-uuid-98753d2f-34d1-41c7-b8dd-a985bf460318-6280196-mszymcza/ex03/ft_str_is_numeric.c
@@ -25,3 +25,14 @@ int	ft_str_is_numeric(char *str)
 	}
 	return (1);
 }
+/*
+#include <stdio.h>
+
+int    main(void)
+{
+    int is_numeric;
+    char a[] = "abc";
+    is_numeric = ft_str_is_numeric(a);
+    printf("%d\n", is_numeric);
+}
+*/
